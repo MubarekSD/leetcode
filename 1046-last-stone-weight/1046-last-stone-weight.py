c3188311dacc8b919e8ba3cae997efefe3ba0aeb
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-03 15:52:00 +0300

    Time: 54 ms (5.87%), Space: 13.9 MB (48.94%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sorts(self, stones: List[int]) -> List[int]:
+        length = len(stones)
+        for i in range(length):
+            for j in range(length - 1):
+                if stones[j] > stones[j + 1]:
+                    stones[j], stones[j + 1] = stones[j + 1], stones[j]
+        return stones
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        while(len(stones) >= 2):
+            self.sorts(stones)
+            temp = stones[-1] - stones[-2]
+            stones.remove(stones[-1])
+            stones.remove(stones[-1])
+            stones.append(temp)
+
+        return stones[0]
