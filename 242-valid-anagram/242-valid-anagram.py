d6eea4233414084ee69886239a34e0f313293c96
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-14 18:08:27 +0300

    Time: 63 ms (32.03%), Space: 16.8 MB (29.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,15 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        aa = dict()
-        bb = dict()
+        aa, bb = dict(), dict()
         for i in range(len(s)):
-            if s[i] not in aa.keys():
-                aa.setdefault(s[i], 1)
-            if t[i] not in bb.keys():
-                bb.setdefault(t[i], 1)
-            aa[s[i]] += 1
-            bb[t[i]] += 1
+            aa[s[i]] = 1 + aa.get(s[i], 0)
+            bb[t[i]] = 1 + bb.get(t[i], 0)
         for i in aa:
             if i not in bb.keys() or aa[i] != bb[i]:
                 return False
